191f52893fc089d8b95306272c33996ff45aa9c0
toshiba <Anass-Rhazal>
2017-03-25 07:58:51 +0000

    Final version

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!--   CSS  File  -->   
+  <!--   CSS  File  -->
 
 
     <link rel="stylesheet" href="css/bootstrap.min.css"  type="text/css">
@@ -13,7 +13,7 @@
 <link rel="stylesheet" href="css/style.css"  type="text/css">
 
  <link rel="stylesheet" href="font-awesome-4.4.0/css/font-awesome.min.css"  type="text/css">
- 
+
 
 </head>
 <body>
@@ -26,15 +26,18 @@
 session_start();
 
 if(isset($_POST['ok'])) {
-      $user=$_POST['user'];   
-      
-      $pass=$_POST['pass'];   
+      $user=$_POST['user'];
+
+      $pass=$_POST['pass'];
    if($user=="admin" && $pass=="admin") {
+    setcookie("user",$user,time()+3600*24);
+		setcookie("pass",$pass,time()+3600*24);
+
 
      $_SESSION['ok']=1;
 
 
-   } 
+   }
   else header("location:admin.html");
 
 
@@ -57,7 +60,7 @@ if(isset( $_SESSION['ok'])){
 <td style="text-align: center;">MAJ</td>
 </thead>
 <tbody>
- 
+
 <?php
 //var_dump(mail("anass@mailinator.com", "test", "ceci est un email de test"));
 $con=new pdo("mysql:host=localhost;dbname=geni;","root","root");
@@ -66,10 +69,15 @@ $pre=$con->prepare("select * from message");
 
 
 $pre->execute();
-
+$x="<?xml version='1.0' encoding='UTF-8'?>
+      <users>  ";
 while($res=$pre->fetch()) {
 
-
+$x.="<user>
+     <name>". $res['nom']."</name>
+     <message>". $res['msg']."</message>
+     <email>". $res['email']."</email>
+     </user> ";
 ?>
 
 
@@ -82,7 +90,7 @@ while($res=$pre->fetch()) {
 
 
 
-<td> <a  class="btn btn-warning circle"   data-toggle="tooltip" data-placement="left" title="Répondre" href="#"><span class="glyphicon glyphicon-pencil"></span></a> 
+<td> <a  class="btn btn-warning circle"   data-toggle="tooltip" data-placement="left" title="Répondre" href="#"><span class="glyphicon glyphicon-pencil"></span></a>
 <a class="btn btn-danger circle" data-toggle="tooltip" data-placement="left" title="Supprimer" href="delete.php?id=<?php echo $res['id']; ?>"><span class="glyphicon glyphicon-trash"></span></a></td>
 
 </tr>
@@ -97,12 +105,11 @@ while($res=$pre->fetch()) {
 <?php
 
 }
+$x.="</users>";
 $con="";
 
  ?>
 
-
-  
 
 
 
@@ -114,29 +121,46 @@ $con="";
 </tbody>
 </table>
 </div>
+<form action="admin.php" method="post">
 
-<?php 
+   <button type="submit" name="download" id="download" class="btn"> <span class="fa fa-download"></span></button>
+
+</form>
+<?php
 }
 
 
 
- else header("location:admin.html"); 
+ else header("location:admin.html");
 
+
+
+if(isset($_POST['download'])) {
+
+
+$dom = new DOMDocument;
+$dom->preserveWhiteSpace = FALSE;
+$dom->loadXML($x);
+
+
+$dom->save('xml/message.xml');
+
+}
 ?>
 
 
 
  <script type="text/javascript" src="old/jquery-2.1.1.js"></script>
-   
 
-	<script type="text/javascript" src="js/bootstrap.min.js"></script> 
-   
+
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
 
    <script>
 
 
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
+    $('[data-toggle="tooltip"]').tooltip();
 });
 </script>
 
